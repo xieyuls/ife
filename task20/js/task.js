ac1b221dfbbdee79278a7a5d9d818aee17f4775f
xieyuls <410880162@qq.com>
2017-04-01 10:06:57 +0800

    Update task.js

--- task20/js/task.js
+++ task20/js/task.js
@@ -1,55 +1,44 @@
 /**
  * Created by Xieyu on 2016/12/19.
  */
-var aqidata=[];
-
-function init(){
-
-
-
-    document.getElementById("in").addEventListener("click",inHandle);
-    document.getElementById("check").addEventListener("click",search);
-
-}
-
-function inHandle(){
-    var value=document.getElementById("content").value;
-    aqidata=value.split(/[^0-9a-zA-Z\u4e00-\u9fa5]+/);
-    console.log(aqidata);
-    render();
-}
-
-function render(){
-    var show=document.getElementById("show");
-    show.innerHTML="";
-    for(var i=0;i<aqidata.length;i++){
-        var node=document.createElement("div");
-        node.style.background="red";
-        node.style.color="white";
-        node.style.textAlign="center";
-        node.style.margin="5px";
-        node.style.display="inline-block";
-        node.style.overflow="hidden";
-        node.setAttribute("class","node"+i);
-        node.innerText=aqidata[i];
-        show.appendChild(node);
-    }
-}
-
-function search() {
-    var info=document.getElementById("search").value;
-    var show=document.getElementById("show");
-    var childs=show.childNodes;
-    for(var i=0;i<childs.length;i++){
-        if(childs[i].innerText.indexOf(info.toString())!=-1){
-            childs[i].style.background="blue";
-            for(var j=0;j<childs.length;j++) {
-                if(i==j) continue;
-                childs[j].style.background="red";
+    var aqidata=[];
+    //将事件封装进一个构造函数内，避免污染全局变量
+    var Zhang=function(){
+        this.inHandle=function () {
+            //先获取textarea的值，然后将其存入数组中
+            var value=document.getElementById("content").value;
+            aqidata=value.split(/[^0-9a-zA-Z\u4e00-\u9fa5]+/);
+            //获取展示区域，在其中创建元素
+            var show=document.getElementById("show");
+            show.innerHTML="";
+            for(var i=0;i<aqidata.length;i++){
+                var node=document.createElement("div");
+                node.style.background="red";
+                node.style.color="white";
+                node.style.textAlign="center";
+                node.style.margin="5px";
+                node.style.display="inline-block";
+                node.style.overflow="hidden";
+                node.setAttribute("class","node"+i);
+                node.innerText=aqidata[i];
+                show.appendChild(node);
+            }
+        };
+        this.search=function() {
+            var info=document.getElementById("search").value;
+            var show=document.getElementById("show");
+            var childs=show.childNodes;
+            for(var i=0;i<childs.length;i++){
+                if(childs[i].innerText.indexOf(info.toString())!=-1){
+                    childs[i].style.background="blue";
+                    for(var j=0;j<childs.length;j++) {
+                        if(i==j) continue;
+                        childs[j].style.background="red";
+                    }
+                }
             }
         }
-    }
-    console.log(childs);
-}
-
-init();
+    };
+    var zhang=new Zhang();
+    document.getElementById("in").addEventListener("click",zhang.inHandle);
+    document.getElementById("check").addEventListener("click",zhang.search);
